1b9dccb2ee4cf95587e92188063a6603548562f6
Miles Wildmore <mileswildmore18@gmail.com>
2024-11-08 13:11:42 -0500

    Added more folders for the utilities to be active while on the main page from the src folder. Also made changes to the CSS file for index to make the website responsive.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      // Determine the colors
+      colors: {
+        blue: '#2997FF',
+        gray: {
+          DEFAULT: "86868b",
+          100: "#94928d",
+          200: "#afafaf",
+          300: "#42424570",
+        },
+        zinc: "101010",
+      },
+    },
   },
   plugins: [],
 }
