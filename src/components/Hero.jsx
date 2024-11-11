50b49c221593b602e2dc1bb62ea1a5d21d858498
Miles Wildmore <mileswildmore18@gmail.com>
2024-11-11 13:41:32 -0500

    Added a video animation of iPhone for when it appears on big and small screens along with giving it the response when user resizes the page with useEffect properties.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,29 @@
 // Add the Hero Page and GSAP effects
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-
+// Import video for large and small devices
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useEffect, useState } from "react";
 const Hero = () => {
+    // Add a video
+    const [videoSrc, setVideoSrc] = useState(window.innerWidth <760 ? smallHeroVideo: heroVideo)
+    // Make the video go horizontal on larger screens and vertically on smaller screens
+    const handleVideoSrcSet = () => {
+        if(windows.innerWidth < 760) {
+            setVideoSrc(smallHeroVideo)
+        } else {
+            setVideoSrc(heroVideo)
+        }
+    }
+    // Check for events happening by the user resizing the screen
+    useEffect(() => {
+        window.addEventListener('resize', handleVideoSrcSet);
+
+        // Clear the event after user is done resizing
+        return() => {
+            window.removeEventListener('resize', handleVideoSrcSet)
+        }
+    }, [])
     // Add GSAP effects
     useGSAP(() => {
         // Animate the id to make the words appear with a fading effect
@@ -14,6 +35,13 @@ const Hero = () => {
       {/* Have the height at 83% and push the highlights below*/}
       <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">iPhone 15 Pro</p>
+        <div className="md:w-10/12 w-9/12">
+        {/* Play the video without sound and show the animation without user tampering with it */}
+            <video className="point-event-none" autoPlay muted playsInline={true} key={videoSrc}>
+                {/* Show the video source */}
+               <source src={videoSrc} type="video/mp4" /> 
+            </video>
+        </div>
       </div>
     </section>
   );
