7f7a13cbf00b4f6b81044bd5ea6e65f3afdfca04
Miles Wildmore <mileswildmore18@gmail.com>
2024-11-11 14:41:33 -0500

    Added the top of the highlight section with adding the title along with animation to make the title along with events appear when the user accesses the webpage.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,53 +5,65 @@ import { useGSAP } from "@gsap/react";
 import { heroVideo, smallHeroVideo } from "../utils";
 import { useEffect, useState } from "react";
 const Hero = () => {
-    // Add a video
-    const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo: heroVideo)
-    // Make the video go horizontal on larger screens and vertically on smaller screens
-    const handleVideoSrcSet = () => {
-        if(windows.innerWidth < 760) {
-            setVideoSrc(smallHeroVideo)
-        } else {
-            setVideoSrc(heroVideo)
-        }
+  // Add a video
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+  // Make the video go horizontal on larger screens and vertically on smaller screens
+  const handleVideoSrcSet = () => {
+    if (windows.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
     }
-    // Check for events happening by the user resizing the screen
-    useEffect(() => {
-        window.addEventListener('resize', handleVideoSrcSet);
+  };
+  // Check for events happening by the user resizing the screen
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
 
-        // Clear the event after user is done resizing
-        return() => {
-            window.removeEventListener('resize', handleVideoSrcSet)
-        }
-    }, [])
-    // Add GSAP effects
-    useGSAP(() => {
-        // Animate the id to make the words appear with a fading effect
-        gsap.to('#hero', { opacity: 1, delay: 2 })
-        gsap.to('#cta', {opacity: 1, y: -50, delay: 2 })
-    }, [])
+    // Clear the event after user is done resizing
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
+  // Add GSAP effects
+  useGSAP(() => {
+    // Animate the id to make the words appear with a fading effect
+    gsap.to("#hero", { opacity: 1, delay: 2 });
+    gsap.to("#cta", { opacity: 1, y: -50, delay: 2 });
+  }, []);
   return (
     // Fill up the page and make it appear below the navbar
     <section className="w-full nav-height bg-black relative">
       {/* Have the height at 83% and push the highlights below*/}
       <div className="h-5/6 w-full flex-center flex-col">
-        <p id="hero" className="hero-title">iPhone 15 Pro</p>
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
         <div className="md:w-10/12 w-9/12">
-        {/* Play the video without sound and show the animation without user tampering with it */}
-            <video className="point-event-none" autoPlay muted playsInline={true} key={videoSrc}>
-                {/* Show the video source */}
-               <source src={videoSrc} type="video/mp4" /> 
-            </video>
+          {/* Play the video without sound and show the animation without user tampering with it */}
+          <video
+            className="point-event-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            {/* Show the video source */}
+            <source src={videoSrc} type="video/mp4" />
+          </video>
         </div>
       </div>
 
-        {/* Add animation for item to go from the bottom of the screen to the top of the section in the center of the screen */}
-      <div 
+      {/* Add animation for item to go from the bottom of the screen to the top of the section in the center of the screen */}
+      <div
         id="cta"
-        className="flex flex-col items-center opacity-0 translate-y-20">
-            <a href="#highlights" className="btn">Buy</a>
-            <p className="font-normal text-xl">From $199/month or $999</p>
-
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
   );
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,7 +1,37 @@
-// Highlight Page
+// Add GSAP components
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { rightImg, watchImg } from "../utils"
+// Add the Highlight section
 const Highlights = () => {
+    useGSAP(() => {
+        // Animate the title and links in the Highlight section
+        gsap.to('#title', { opacity: 1, y: 0 })
+        // Make links appear slightly behind the other as they appear
+        gsap.to('.link',{ opacity: 1, y: 0, duration: 1, stagger: 0.25 })
+    }, [])
+
   return (
-    <div>Highlights</div>
+    // Fill the entire width and height of the section with no scroll bar
+    <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
+        <div className="screen-max-width">
+            {/* Show highlights on the left and events on the right on large and medium deices */}
+            <div className="mb-12 w-full md:flex items-end justify-between">
+                <h1 id="title" className="section-heading">Get the highlights!</h1>
+                {/* Add events that will be spread out from each other */}
+                <div className="flex flex-wrap items-end gap-5">
+                    <p className="link">Watch the film
+                        {/* Add image for watching the film*/}
+                        <img src={watchImg} alt="watch" className="ml-2" />
+                    </p>
+                    <p className="link">Watch the event
+                        {/* Add image for watching the event */}
+                        <img src={rightImg} alt="wright" className="ml-2" />
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
