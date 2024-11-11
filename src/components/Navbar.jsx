201f362fb45c7c35216fb16b502fca0ccacdb0a1
Miles Wildmore <mileswildmore18@gmail.com>
2024-11-11 12:10:54 -0500

    Added images for the navigation bar for the website along with searching items, buying items and the logo of Apple.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,29 @@
+//Import pictures for navigation
+import { appleImg, bagImg, searchImg } from "../utils";
+
 // Add a Navbar to the website
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header>
+      <nav>
+        <img src={appleImg} Alt="Apple" width={14} height={18} />
 
-export default Navbar
+        <div>
+            {/* Add Apple products into an array based on navigation */}
+            {['Phones', 'Macbooks', 'Tablets'].map((nav, i) => (
+                <div key={nav}>
+                    {nav}
+                    </div>
+            ))}
+        </div>
+
+        <div>
+            <img src={searchImg} alt="search" width={18} height={18} />
+            <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
