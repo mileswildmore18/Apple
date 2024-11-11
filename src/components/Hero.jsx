bc79a3a34a12bb0eb316c17d545ac3d3c56d5861
Miles Wildmore <mileswildmore18@gmail.com>
2024-11-11 13:15:39 -0500

    Added the GSAP variables based on react, and added word effects with GSAP to fade in when user accesses the index page. Also added a comment in the index css file for hero-title to describe what is happening in the background.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,22 @@
-// Add the Hero Page
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+// Add the Hero Page and GSAP effects
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
-export default Hero
+const Hero = () => {
+    // Add GSAP effects
+    useGSAP(() => {
+        // Animate the id to make the words appear with a fading effect
+        gsap.to('#hero', { opacity: 1, delay: 1.5 })
+    }, [])
+  return (
+    // Fill up the page and make it appear below the navbar
+    <section className="w-full nav-height bg-black relative">
+      {/* Have the height at 83% and push the highlights below*/}
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">iPhone 15 Pro</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
