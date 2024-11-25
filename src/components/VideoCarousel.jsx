4c6854cfdb5485c37dcdbdd8f41ff6abc2b1e020
Miles Wildmore <mileswildmore18@gmail.com>
2024-11-25 16:36:01 -0500

    Added meta data to be triggered once a video starts playing. Trying to fix line 29 to see why the website breaks on gsap.to.

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -2,6 +2,7 @@
 import { useEffect, useRef, useState } from "react";
 import { hightlightsSlides } from "../constants";
 import { pauseImg, playImg, replayImg } from "../utils";
+import { useGSAP } from "@gsap/react";
 
 // Add video carousel properties to display different items
 const VideoCarousel = () => {
@@ -22,8 +23,24 @@ const VideoCarousel = () => {
   // Deconstruct the file
   const { isEnd, isLastVideo, startPlay, videoId, isPLaying } = video;
 
-//   Animate the video playing
-  
+  //Animate the video playing
+  useGSAP(() => {
+    //website breaks here*************************************************************
+    // gsap.to('#video', {
+    //     scrollTrigger: {
+    //         trigger: '#video',
+    //         toggleActions: 'restart none none none'
+    //     },
+    //     onComplete: () => {
+    //         setVideo((prevVideo) => ({
+    //             ...prevVideo,
+    //             startPlay: true,
+    //             isPLaying: true
+    //         }))
+    //     }
+    // })
+    // end of error*********************************************************************
+  }, [isEnd, videoId])
   //Start Current video
   useEffect(() => {
     if (loadedData.length > 3) {
@@ -34,6 +51,11 @@ const VideoCarousel = () => {
       }
     }
   }, [startPlay, videoId, isPLaying, loadedData]);
+
+// new code added Add Meta data to videos*********************************************************************
+  const handleLoadedMetadata = (i, e) => setLoadedData
+  ((pre) => [...pre, e])
+  //end of new code*****************************************************************
 
   // Play the videos
   useEffect(() => {
@@ -51,25 +73,37 @@ const VideoCarousel = () => {
     }
   }, [videoId, startPlay]);
 
-  //Functions of playing the video 
-    const handleProcess = (type, i) => {
-        switch (type) {
-            case 'video-end':
-                setVideo((prevVideo) => ({...prevVideo, isEnd: true, videoId: i + 1 }))
-                break;
-            case 'video-last':
-                setVideo((prevVideo) => ({ ...prevVideo, isLastVideo: true}))
-                break;
-                case 'video-reset':
-                setVideo ((prevVideo) => ({ ...prevVideo, isLastVideo: false, videoId: 0 }))
-                break;
-                case 'play':
-                    setVideo ((prevVideo) => ({ ...prevVideo, isPLaying: !prevVideo.isPLaying, videoId: 0 }))
-                    break;
-            default:
-                return video;
-        }
+  //Functions of playing the video
+  const handleProcess = (type, i) => {
+    switch (type) {
+      case "video-end":
+        setVideo((prevVideo) => ({
+          ...prevVideo,
+          isEnd: true,
+          videoId: i + 1,
+        }));
+        break;
+      case "video-last":
+        setVideo((prevVideo) => ({ ...prevVideo, isLastVideo: true }));
+        break;
+      case "video-reset":
+        setVideo((prevVideo) => ({
+          ...prevVideo,
+          isLastVideo: false,
+          videoId: 0,
+        }));
+        break;
+      case "play":
+        setVideo((prevVideo) => ({
+          ...prevVideo,
+          isPLaying: !prevVideo.isPLaying,
+          videoId: 0,
+        }));
+        break;
+      default:
+        return video;
     }
+  };
   return (
     <>
       {/* Add all videos from the constant index js file to show the videos */}
@@ -93,6 +127,9 @@ const VideoCarousel = () => {
                       isPLaying: true,
                     }));
                   }}
+                //   new code, Trigger the event when Meta data is loaded*****************************************************
+                  onLoadedMetadata={(e) => handleLoadedMetadata(i,e)}
+                //   end of new code******************************************************************************************
                 >
                   <source src={list.video} type="video/mp4" />
                 </video>
@@ -122,22 +159,27 @@ const VideoCarousel = () => {
               ref={(el) => (videoDivRef.current[i] = el)}
               className="mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer"
             >
-            <span className="absolute h-full w-full rounded-full" ref={(el) => (videoSpanRef.current[i] = el)} /> 
+              {/* Makes buttons disappear for some reason on load up */}
+              <span
+                className="absolute h-full w-full rounded-full"
+                ref={(el) => (videoSpanRef.current[i] = el)}
+              />
             </span>
           ))}
         </div>
-          {/* Add button to play or pause */}
+        {/* Add button to play or pause */}
         <button className="control-btn">
-            <img 
-                src={isLastVideo ? replayImg : !isPLaying ? playImg :pauseImg}
-                alt={isLastVideo ? 'replay' : !isPLaying? 'play' : 'pause'}
-                onClick={isLastVideo 
-                    ? () => handleProcess('video-reset')
-                    : !isPLaying
-                        ? () => handleProcess('play')
-                        : () => handleProcess('pause')
-                    }
-            />
+          <img
+            src={isLastVideo ? replayImg : !isPLaying ? playImg : pauseImg}
+            alt={isLastVideo ? "replay" : !isPLaying ? "play" : "pause"}
+            onClick={
+              isLastVideo
+                ? () => handleProcess("video-reset")
+                : !isPLaying
+                ? () => handleProcess("play")
+                : () => handleProcess("pause")
+            }
+          />
         </button>
       </div>
     </>
