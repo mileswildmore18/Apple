9beae4acf27ef5df765003dd4c9a2861345d1732
Miles Wildmore <mileswildmore18@gmail.com>
2024-12-04 11:11:42 -0500

    Installed and added Three JS to the project to add animation for rotating items on the website screen. Added the reference of Three JS to help with camera rotation and control  of particular products of phones and other items. Added a specific model for visual aid. Finally, added the model reference into the ModelView pass important information from the Model itself.

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,11 +1,33 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import ModelView from "./ModelView";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { yellowImg } from "../utils";
+
+// Import Three JS
+import * as THREE from "three";
 
 const Model = () => {
-    // Choose with phone to look at in the model
+    // Choose what phone to look at in the model at small or large size and different color textures
  const [size, setSize ] = useState('small');
+//  Specify model details
+ const [model, setModel ] = useState({
+    title: 'iPhone 15 Pro in Natural Titanium',
+    color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+    img: yellowImg, 
+ });
+
+ // Setup camera control for the model view
+const cameraControlSmall = useRef();
+const cameraControlLarge = useRef();
+
+// Add Three JS to view phones model
+const small = useRef(new THREE.Group());
+const large = useRef(new THREE.Group());
+
+// Add rotation 
+const [smallRotation, setSmallRotation] = useState(0);
+const [largeRotation, setLargeRotation] = useState(0);
   // Add animation to the header
   useGSAP(() => {
     gsap.to("#heading", { y: 0, opacity: 1 });
@@ -16,10 +38,19 @@ const Model = () => {
         <h1 id="heading" className="section-heading">
           Take a closer look.
         </h1>
-
+        {/* View the models of the phones */}
         <div className="flex flex-col item-center mt-5">
             <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-                <ModelView />
+                {/* Pass important information from Model to view phone for reference */}
+                <ModelView 
+                    index={1}
+                    groupRef={small}
+                    gsapType="view1"
+                    controlRef={cameraControlSmall}
+                    setRotationState={setSmallRotation}
+                    item={model}
+                    size={size}
+                />
             </div>
         </div>
       </div>
