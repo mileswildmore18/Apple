26d34e341276120a69980ca6bc93275fea03ed36
Miles Wildmore <mileswildmore18@gmail.com>
2024-12-05 10:37:09 -0500

    Added the start of the view of the Model creating a border around the items shown as well as starting the camera perspective to show the item from the camera to freely move around to see the item. Also added the beginning of the light effects for items in the Model.

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,6 +1,19 @@
-const ModelView = () => {
+import { PerspectiveCamera, View } from "@react-three/drei"
+
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item }) => {
   return (
-    <div>ModelView</div>
+    // View each item in the model
+    <View
+    index={index}
+    id={gsapType}
+    className={`border-2 border-red-500 w-full h-full ${index === 2} ? 'right-[-100%] : ''`}>
+        {/* Ambient Light */}
+        <ambientLight intensity={0.3} />
+
+        {/* Create moving camera */}
+        <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+    </View>
   )
 }
 
