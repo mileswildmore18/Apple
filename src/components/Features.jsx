5aa0c5c44a0b068ef8bfae6037b850e60bd30ef1
Miles Wildmore <mileswildmore18@gmail.com>
2024-12-06 17:06:08 -0500

    Added a video to the features and animated to make it play when user scrolls to the video. Next is to add the other videos to the feature section.

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,19 +1,37 @@
 import { useGSAP } from '@gsap/react';
-import React from 'react'
+import React, { useRef } from 'react'
 import { animateWithGsap } from '../utils/animations';
+import { exploreVideo } from '../utils';
 
 const Features = () => {
+    // Trigger the video to play after scrolling to it
+    const videoRef = useRef(); 
     // Use gsap to animate the header
     useGSAP(() => {
        animateWithGsap('#features_title',{y: 0, opacity: 1})
     },[]);
 
   return (
-    <section className='h-full common-padding bg-zinc relative overflow-hidden'>Features
+    <section className='h-full common-padding bg-zinc relative overflow-hidden'>
     <div className='screen-max-width'>
        <div className='mb-12 w-full'>
         <h1 id='features_title' className='section-heading'>Explore the full story.</h1>
-        </div> 
+        </div>
+        <div className='flex-flex-col justify-center items-center overflow-hidden'>
+            <div className='mt-32 mb-24 pl-24'>
+                <h2 className='text-5xl lg:text-7xl font-semibold'>iPhone.</h2>
+                <h2 className='text-5xl lg:text-7xl font-semibold'>Forged in Titanium</h2>
+            </div>
+            {/* View the content of the video */}
+            <div className='flex-center flex-col sm:px-10'>
+                <div className="relative h-[50vh] w-full flex items-center">
+                    {/* Play the video when user scrolls to the video */}
+                    <video playsInline id="exploreVideo" className='w-full h-full object-cover object-center' preload='none' muted autoPlay ref={videoRef}>
+                        <source src={exploreVideo} type="video/mp4" />
+                    </video>
+                </div>
+            </div>
+        </div>
     </div>
     </section>
   )
