718d0d003f6bb2e99ddc903d0afc22df2d80bc13
Miles Wildmore <mileswildmore18@gmail.com>
2024-12-11 13:17:12 -0500

    Added a phone border for video play in the HowItWorks section. Also added the video to fit into the border screen and animate the video to making it play.

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,33 +1,70 @@
-import React from 'react'
-import { chipImg } from '../utils'
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap';
+import React, { useRef } from "react";
+import { chipImg, frameImg, frameVideo } from "../utils";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const HowItWorks = () => {
-    useGSAP(() => {
-        // Animate the image of the chip easing into frame from large size to normal size
-      gsap.from('#chip', {
-        scrollTrigger: {
-            trigger: '#chip',
-            start: '20% bottom'
-        },
-        opacity: 0,
-        scale: 2,
-        duration: 2,
-        ease: 'power2.inOut'   
-    })
-}, []);
+    // Animate the video for HowItWorks
+  const videoRef = useRef();
+  useGSAP(() => {
+    // Animate the image of the chip easing into frame from large size to normal size
+    gsap.from("#chip", {
+      scrollTrigger: {
+        trigger: "#chip",
+        start: "20% bottom",
+      },
+      opacity: 0,
+      scale: 2,
+      duration: 2,
+      ease: "power2.inOut",
+    });
+  }, []);
 
   return (
     // Add a image showing the chip in the center of the section
     <section className="common-padding">
-        <div className='screen=max-width'>
-            <div id='chip' className='flex-center w-full my-20'>
-                <img src={chipImg} alt='chip' width={180} height={180} />
-            </div>
+      <div className="screen=max-width">
+        <div id="chip" className="flex-center w-full my-20">
+          <img src={chipImg} alt="chip" width={180} height={180} />
         </div>
+        {/* Add details about the featured item */}
+        <div className="flex flex-col items-center">
+          <h2 className="hiw-title">
+            A17 Pro chip.
+            <br /> A monster win for gaming.
+          </h2>
+          <p className="hiw-subtitle">
+            It's here. The biggest redesign in the history of Apple GPUs.
+          </p>
+        </div>
+        {/* Display the border of a phone for the video to play in*/}
+        <div className="mt-10 md:mt-20 mb-14">
+          <div className="relative h-full flex-center">
+            <div className="overflow-hidden">
+              <img
+                src={frameImg}
+                alt="frame"
+                className="bg-transparent relative z-10"
+              />
+            </div>
+            {/* Display the video */}
+            <div className="hiw-video">
+              <video
+                className="point-events-none"
+                playsInline
+                preload="none"
+                muted
+                autoPlay
+                ref={videoRef}
+              >
+                <source src={frameVideo} type="video/mp4" />
+              </video>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default HowItWorks
+export default HowItWorks;
